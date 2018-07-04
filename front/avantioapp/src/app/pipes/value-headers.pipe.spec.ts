ee9d01d5e337fe0ffb95cc66d919c75a39a2fc5b
Javier <jsm.multimedia@gmail.com>
2018-07-04 21:07:23 +0200

    Testing #1

--- front/avantioapp/src/app/pipes/value-headers.pipe.spec.ts
+++ front/avantioapp/src/app/pipes/value-headers.pipe.spec.ts
@@ -1,8 +1,17 @@
+import { TestBed, inject, async } from '@angular/core/testing';
 import { ValueHeadersPipe } from './value-headers.pipe';
-
+import {ApiServeService} from '../services/api-serve.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { RestangularModule, Restangular, RestangularHttp } from 'ngx-restangular';
 describe('ValueHeadersPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ValueHeadersPipe();
-    expect(pipe).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RestangularModule],
+      providers: [ Restangular,ApiServeService]
+    });
   });
+  it('create an instance', inject([ Restangular ,HttpTestingController,ApiServeService ], (_RestangularHttp:Restangular ,_HttpTestingController:HttpTestingController,_ApiServeService: ApiServeService) => {
+    const pipe = new ValueHeadersPipe(_ApiServeService);
+    expect(pipe).toBeTruthy();
+  }));
 });
